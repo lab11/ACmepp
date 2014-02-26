534d846642401a67c5a307037a4c8a36e023f5f2
Brad Campbell <bradjc5@gmail.com>
2014-02-26 00:47:37 -0500

    fixed leds
    they were backwards before (on was off)

--- contiki/platform/acme++/dev/leds-arch.c
+++ contiki/platform/acme++/dev/leds-arch.c
@@ -4,6 +4,9 @@
 #include "dev/leds.h"
 #include "dev/gpio.h"
 
+/* Invert all of the leds because they are active low.
+ */
+
 #define LEDS_GPIO_PIN_MASK   LEDS_ALL
 /*---------------------------------------------------------------------------*/
 void
@@ -17,17 +20,17 @@ leds_arch_init(void)
 unsigned char
 leds_arch_get(void)
 {
-	return GPIO_READ_PIN(LED_BLUE_BASE,  LED_BLUE_MASK) |
+	return ~(GPIO_READ_PIN(LED_BLUE_BASE,  LED_BLUE_MASK) |
 	       GPIO_READ_PIN(LED_RED_BASE,   LED_RED_MASK) |
-           GPIO_READ_PIN(LED_GREEN_BASE, LED_GREEN_MASK);
+           GPIO_READ_PIN(LED_GREEN_BASE, LED_GREEN_MASK));
 }
 /*---------------------------------------------------------------------------*/
 void
 leds_arch_set(unsigned char leds)
 {
-	GPIO_WRITE_PIN(LED_BLUE_BASE,  LED_BLUE_MASK,  leds);
-	GPIO_WRITE_PIN(LED_RED_BASE,   LED_RED_MASK,   leds);
-	GPIO_WRITE_PIN(LED_GREEN_BASE, LED_GREEN_MASK, leds);
+	GPIO_WRITE_PIN(LED_BLUE_BASE,  LED_BLUE_MASK,  ~leds);
+	GPIO_WRITE_PIN(LED_RED_BASE,   LED_RED_MASK,   ~leds);
+	GPIO_WRITE_PIN(LED_GREEN_BASE, LED_GREEN_MASK, ~leds);
 }
 /*---------------------------------------------------------------------------*/
 
